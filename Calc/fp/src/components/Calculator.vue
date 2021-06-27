b511a92032ce71e6439262aa41f4fade66be516c
Alexander <62078595+En1ere@users.noreply.github.com>
2021-06-27 14:28:24 +0500

    2

--- Calc/fp/src/components/Calculator.vue
+++ Calc/fp/src/components/Calculator.vue
@@ -2,17 +2,18 @@
   <div>
     <input type="number" v-model.number="operand1" placeholder="operand1" />
     <input type="number" v-model.number="operand2" placeholder="operand2" />
-    = {{ sum }}
+    = {{ result }}
 
     <div class="keyboard">
-      <button v-on:click="sum = operand1 + operand2">+</button>
-      <button v-on:click="sum = operand1 - operand2">-</button>
+      <button v-on:click="result = operand1 + operand2">+</button>
+      <button v-on:click="result = operand1 - operand2">-</button>
       <button @click="mult">*</button>
       <button @click="mult">=</button>
       <button @click="div(operand1, operand2)">/.</button>
       <button @click="exponentiation(operand1, operand2)">^</button>
       <button @click="intDiv(operand1, operand2)">/</button>
     </div>
+    {{ error }}
   </div>
 </template>
 
@@ -23,22 +24,31 @@ export default {
     return {
       operand1: 0,
       operand2: 0,
-      sum: 0,
+      result: 0,
+      error: "",
     };
   },
   methods: {
     mult() {
-      this.sum = this.operand1 * this.operand2;
+      this.result = this.operand1 * this.operand2;
     },
     div(op1, op2) {
-      this.sum = op1 / op2;
+      const { operand1 } = this;
+      if (operand1 === 0) {
+        this.error = "На 0 делить нельзя";
+      }
+      this.result = op1 / op2;
     },
     exponentiation(op1, op2) {
-      this.sum = Math.pow(op1, op2);
+      this.result = Math.pow(op1, op2);
     },
     intDiv(op1, op2) {
-        this.sum = Math.floor(op1 / op2)
-    }
+      const { operand1 } = this;
+      if (operand1 === 0) {
+        this.error = "На 0 делить нельзя";
+      }
+      this.result = Math.floor(op1 / op2);
+    },
   },
 };
 </script>
