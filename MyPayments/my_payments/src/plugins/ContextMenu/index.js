eb6a0b58bad0a3469c6dc9d4afe39f9092889d5e
Alexander <62078595+En1ere@users.noreply.github.com>
2021-07-18 13:17:29 +0500

    1

---
+++ MyPayments/my_payments/src/plugins/ContextMenu/index.js
@@ -0,0 +1,17 @@
+export default {
+    install(Vue) {
+        if (this.installed) {
+            return
+        }
+
+        this.installed = true
+
+        Vue.prototype.$context = {
+            EventBus: new Vue(),
+
+            show(name, settings) {
+                this.EventBus.$emit('show', {name, settings})
+            },
+        }
+    }
+}
